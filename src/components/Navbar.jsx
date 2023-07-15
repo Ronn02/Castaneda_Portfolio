4e994c66cf17c1884b007e846c1326ba90c1b2b5
Ronn02 <72732177+Ronn02@users.noreply.github.com>
2023-07-15 17:09:27 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#08192f] text-gray-300'>
         <div>
-            <img src={Logo} alt="Logo Image" style = {{width: ' 100px'}}/>
+            <Link to='home' smooth={true} duration={500} ><img src={Logo} alt="Logo Image" style = {{width: ' 100px'}}/></Link>
         </div>
         {/*menu*/}
         
